5fc2fe969f24091acb7bd977e293edb9eadb9218
billmcculloch <billrmcculloch@gmail.com>
2024-07-28 20:37:16 +0100

    Fix spacing

--- app/components/Hero/index.tsx
+++ app/components/Hero/index.tsx
@@ -17,32 +17,34 @@ function Hero() {
 
   return (
     <div className={s.wrapper}>
-      <Image
-        src={profile}
-        alt="mobile profile picture"
-        className={s.mobileProfile}
-        width={90}
-        height={90}
-        priority
-      />
-      <div className={s.text}>
-        <h3>Hello, I'm</h3>
-        <div className={s.hero}>
-          <Image
-            src={profile}
-            alt="profile pictre"
-            className={s.profile}
-            width={120}
-            height={120}
-            priority
-          />
-          <h1>Bill McCulloch</h1>
+      <div className={s.container}>
+        <Image
+          src={profile}
+          alt="mobile profile picture"
+          className={s.mobileProfile}
+          width={90}
+          height={90}
+          priority
+        />
+        <div className={s.text}>
+          <h3>Hello, I'm</h3>
+          <div className={s.hero}>
+            <Image
+              src={profile}
+              alt="profile pictre"
+              className={s.profile}
+              width={120}
+              height={120}
+              priority
+            />
+            <h1>Bill McCulloch</h1>
+          </div>
+          <h5>I like to build things and occasionally access the mainframe.</h5>
         </div>
-        <h5>I like to build things and occasionally access the mainframe.</h5>
-      </div>
-      <div className={s.cta} onClick={() => scrollTo("work")}>
-        <p> See what I've been upto</p>
-        <Image src={arrow} alt="arrow" className={s.arrow} />
+        <div className={s.cta} onClick={() => scrollTo("work")}>
+          <p> See what I've been upto</p>
+          <Image src={arrow} alt="arrow" className={s.arrow} />
+        </div>
       </div>
     </div>
   );
